570b7bf3b7c3ef242d2b92ca89b67721fe926293
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2025-02-21 11:42:37 -0300

    feat: ensure cart total is shown

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,6 +11,7 @@ export interface CartProduct
 export interface ICartContext {
   isOpen: boolean;
   products: CartProduct[];
+  total: number;
   toggleCart: () => void;
   addProduct: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
@@ -21,6 +22,7 @@ export interface ICartContext {
 export const CartContext = createContext<ICartContext>({
   isOpen: false,
   products: [],
+  total: 0,
   toggleCart: () => {},
   addProduct: () => {},
   decreaseProductQuantity: () => {},
@@ -31,6 +33,11 @@ export const CartContext = createContext<ICartContext>({
 export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const total = products.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+  }, 0);
+
   const toggleCart = () => setIsOpen((prev) => !prev);
 
   const addProduct = (product: CartProduct) => {
@@ -93,6 +100,7 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
       value={{
         isOpen, // shorthand operator
         products,
+        total,
         toggleCart,
         addProduct,
         decreaseProductQuantity,
