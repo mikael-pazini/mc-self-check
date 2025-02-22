2dc98a1ef140b0c71a8066f9d0eac6ef02589ff8
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2025-02-21 21:51:15 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.ts
+++ src/app/[slug]/menu/actions/create-order.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -55,4 +56,5 @@ export const createOrder = async (input: CreateOrderInput) => {
       restaurantId: restaurant.id,
     },
   });
+  redirect(`/${input.slug}/orders`);
 };
