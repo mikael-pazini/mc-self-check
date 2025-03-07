1a38ff18c51a1a990735e42a71d3e8c1b9b416bb
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2025-03-07 10:28:11 -0300

    feat: create a homepage for the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <div className="flex flex-col items-center gap-8 py-8">
+      <h1 className="text-3xl font-bold">Bem-vindo ao FSW Donalds</h1>{" "}
+      {/*Welcome to FSW Donalds */}
+      <p className="text-center text-lg">
+        Peça sua comida e bebida favorita no FSW Donalds mais perto de você.
+        {/*Order your favorite food and drinks from your nearest FSW Donalds.*/}
+      </p>
+      <p className="text-center text-lg">
+        Escolha o seu restaurante abaixo.
+        {/*Choose your restaurant below.*/}
+      </p>
+      <div>
+        <Button variant="destructive" className="rounded-full" asChild>
+          <Link href={`/fsw-donalds`}>FSW Donalds</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
